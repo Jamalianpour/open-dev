2a7cfc48e889c0dde7cf6310bd951d1e94de8a71
Jamalianpour <Jamalianpour@users.noreply.github.com>
2026-05-23 17:42:52 +0000

    deploy: ac2305c68ed1990fc714a1a11e2f325dfc9ed2e6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3010787758" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3863264282" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
